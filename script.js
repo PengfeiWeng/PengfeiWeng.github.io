943bf5203e97e272ecdb2760969b6c53958924b2
PengfeiWeng <jason.wengpengfei@gmail.com>
2014-12-05 14:28:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ iframe.attr('src', "https://angularjs.org/");
 $('#test').append('<iframe class="temp2"></iframe>');
 $('#temp2').attr('src', "https://angularjs.org/");
 $('<iframe />').attr('src', 'http://www.google.com'); 
+
+console.log("fuck");
