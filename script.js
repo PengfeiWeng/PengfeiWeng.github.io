b9ce3521ba7140c620879cfe19db7e795267ec1a
PengfeiWeng <jason.wengpengfei@gmail.com>
2014-12-05 14:39:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var iframe = $('<iframe class="oAuthIFrame"></iframe>').appendTo('body');
-iframe.attr('src', "https://angularjs.org/");
-
-$('#test').append('<iframe class="temp2"></iframe>');
-$('#temp2').attr('src', "https://angularjs.org/");
-$('<iframe />').attr('src', 'http://www.google.com'); 
-
-console.log("fuck");
+ /**JQuery IFrame **/
+            var iframe = $('<iframe class="oAuthIFrame"></iframe>').appendTo('.signIn');
+            iframe.attr('src', 'https://angularjs.org/');
